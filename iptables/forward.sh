b70a672a2512579c477ea67c057049823f033869
James Zhu <zhujian0805@gmail.com>
2016-06-29 13:51:52 +0800

    M forward.sh

--- iptables/forward.sh
+++ iptables/forward.sh
@@ -23,9 +23,15 @@ service iptables restart
 iptables -F
 iptables -X
 iptables -Z
-iptables -t nat -A PREROUTING -i eth0 -p tcp -d 33129.248.70 --dport 80 -j LOG --log-level 1 --log-prefix 'HTTPForward: '
-iptables -t nat -A PREROUTING -p tcp -d 33129.248.70 --dport 80 -j DNAT --to-destination 33129.248.244
-iptables -t nat -A POSTROUTING -p tcp --dst 33129.248.244 --dport 80 -j SNAT --to-source 33129.248.70
-iptables -t nat -A OUTPUT --dst 33129.248.70 -p tcp --dport 80 -j DNAT --to-destination 33129.248.244
+/sbin/iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 389 -j LOG --log-prefix 'LDAPForward: '
+/sbin/iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 389 -j DNAT --to 10.130.0.81:389
+/sbin/iptables -t nat -A POSTROUTING -o eth0 -p tcp -d 10.130.0.81 --dport 389 -j SNAT --to-source 172.31.248.244
 
+/sbin/iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 636 -j LOG --log-prefix 'LDAPForward: '
+/sbin/iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 636 -j DNAT --to 10.130.0.81:636
+/sbin/iptables -t nat -A POSTROUTING -o eth0 -p tcp -d 10.130.0.81 --dport 636 -j SNAT --to-source 172.31.248.244
 
+/sbin/iptables -t nat -A PREROUTING -i eth0 -p tcp -d 172.31.248.244 --dport 80 -j LOG --log-level 1 --log-prefix 'HTTPForward: '
+iptables -t nat -A PREROUTING -p tcp -d 172.31.248.244 --dport 80 -j DNAT --to-destination 10.47.5.242
+iptables -t nat -A POSTROUTING -p tcp --dst 10.47.5.242 --dport 80 -j SNAT --to-source 172.31.248.244
+iptables -t nat -A OUTPUT --dst 172.31.248.244 -p tcp --dport 80 -j DNAT --to-destination 10.47.5.242
