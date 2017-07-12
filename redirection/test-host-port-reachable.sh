765f6815e066cf04756dfdf7d9d4b045a95bdb00
James Zhu <zhujian0805@gmail.com>
2017-07-12 15:03:35 +0800

    This commit has the following change(s): ------------------------------------------------ A  test-host-port-reachable.sh ------------------------------------------------

---
+++ redirection/test-host-port-reachable.sh
@@ -0,0 +1,29 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: test-host-port-reachable.sh
+# 
+#         USAGE: ./test-host-port-reachable.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: James Zhu (), zhujian0805@gmail.com
+#  ORGANIZATION: ZJ
+#       CREATED: 07/12/2017 15:01
+#      REVISION:  ---
+#===============================================================================
+
+# If host is a valid hostname or Internet address, and port is an integer port number or service name, bash attempts to open the corresponding TCP socket.
+#echo somthing > /dev/tcp/host/port
+echo somthing > /dev/tcp/localhost/22
+echo $?
+
+# If host is a valid hostname or Internet address, and port is an integer port number or service name, bash attempts to open the corresponding UDP socket.
+#echo something > /dev/udp/host/port
+echo somthing > /dev/tcp/localhost/22
+echo $?
+
