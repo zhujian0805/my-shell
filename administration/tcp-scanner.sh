5eb0a3ee62c8be5e74d5195f0fea38a56486a592
James Zhu <zhujian0805@gmail.com>
2017-07-12 16:17:32 +0800

    This commit has the following change(s): ------------------------------------------------  M tcp-scanner.sh ------------------------------------------------

--- administration/tcp-scanner.sh
+++ administration/tcp-scanner.sh
@@ -1,3 +1,5 @@
+# Scripts
+# http://www.catonmat.net/blog/tcp-port-scanner-in-bash/
 for port in {1..65535}; do
     echo >/dev/tcp/baidu.com/$port &&
     echo "port $port is open" || echo "port $port is closed"
