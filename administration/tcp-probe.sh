f50412794c6fa16a1c2c3c50de728faedc9b4b2f
James Zhu <zhujian0805@gmail.com>
2017-07-26 16:18:07 +0800

    This commit has the following change(s): ------------------------------------------------  M tcp-probe.sh ------------------------------------------------

--- administration/tcp-probe.sh
+++ administration/tcp-probe.sh
@@ -1,15 +1,15 @@
 LOG=${0}.log
 exec 6>&1
-exec >> ${LOG} 2>&1
+exec >> "${LOG}" 2>&1
 
 while true
 do
     if timeout 1 bash -c 'echo > /dev/tcp/10.113.192.11/22' 
     then
-        echo -n $(date) tcp socket made succesfully to 10.113.192.11 22
+        echo -n "$(date)" tcp socket made succesfully to 10.113.192.11 22
         echo
     else
-        echo -n $(date) tcp socket made failed to 10.113.192.11 22
+        echo -n "$(date)" tcp socket made failed to 10.113.192.11 22
         echo
     fi
     sleep 1
