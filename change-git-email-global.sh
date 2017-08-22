32d6827b492372e5e1c4066a9e9a0c58ea7d498d
James Zhu <zhujian0805@gmail.com>
2017-08-22 17:41:00 +0800

    This commit has the following change(s): ------------------------------------------------  M change-git-email-global.sh ------------------------------------------------

--- change-git-email-global.sh
+++ change-git-email-global.sh
@@ -6,14 +6,14 @@ CORRECT_EMAIL=$3
 
 git filter-branch -f --env-filter "
 
-if [ \"$GIT_COMMITTER_EMAIL\" = \"$OLD_EMAIL\" ]
+if [ $GIT_COMMITTER_EMAIL = $OLD_EMAIL ]
 then
-    export GIT_COMMITTER_NAME=\"$CORRECT_NAME\"
-    export GIT_COMMITTER_EMAIL=\"$CORRECT_EMAIL\"
+    export GIT_COMMITTER_NAME=$CORRECT_NAME
+    export GIT_COMMITTER_EMAIL=$CORRECT_EMAIL
 fi
-if [ \"$GIT_AUTHOR_EMAIL\" = \"$OLD_EMAIL\" ]
+if [ $GIT_AUTHOR_EMAIL = $OLD_EMAIL ]
 then
-    export GIT_AUTHOR_NAME=\"$CORRECT_NAME\"
-    export GIT_AUTHOR_EMAIL=\"$CORRECT_EMAIL\"
+    export GIT_AUTHOR_NAME=$CORRECT_NAME
+    export GIT_AUTHOR_EMAIL=$CORRECT_EMAIL
 fi
 " --tag-name-filter cat -- --branches --tags
