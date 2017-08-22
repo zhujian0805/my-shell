ea57a83c5b903d7897a7134853abcbfc06f63bd6
James Zhu <zhujian0805@gmail.com>
2017-08-22 20:59:01 +0800

    This commit has the following change(s): ------------------------------------------------  M change-git-email-global.sh ------------------------------------------------

--- change-git-email-global.sh
+++ change-git-email-global.sh
@@ -1,20 +1,37 @@
 #!/bin/sh
 
-
-git filter-branch -f --env-filter "
-
-OLD_EMAIL=oldmail@company.com
-CORRECT_NAME=Names
-CORRECT_EMAIL=newmail@company.com
-
-if [ $GIT_COMMITTER_EMAIL = $OLD_EMAIL ]
+if [ $# -ne 3 ]
 then
-    export GIT_COMMITTER_NAME=$CORRECT_NAME
-    export GIT_COMMITTER_EMAIL=$CORRECT_EMAIL
+    echo "You must specify 3 parameters!!!"
+    echo USAGE:
+    echo "./$0 OLD_EMAIL CORRECT_EMAIL CORRECT_EMAIL"
+    echo
+    echo
+    exit
 fi
-if [ $GIT_AUTHOR_EMAIL = $OLD_EMAIL ]
-then
-    export GIT_AUTHOR_NAME=$CORRECT_NAME
-    export GIT_AUTHOR_EMAIL=$CORRECT_EMAIL
-fi
-" --tag-name-filter cat -- --branches --tags
+
+OLD_EMAIL=$1
+CORRECT_NAME=$2
+CORRECT_EMAIL=$3
+
+SCRIPT_NAME=/tmp/fuckyou.sh
+
+echo '#!/bin/sh' >$SCRIPT_NAME
+echo >>$SCRIPT_NAME
+echo 'git filter-branch -f --env-filter "' >>$SCRIPT_NAME
+echo 'if [ $GIT_COMMITTER_EMAIL =' "$OLD_EMAIL ]" >>$SCRIPT_NAME
+echo then >>$SCRIPT_NAME
+echo "    export GIT_COMMITTER_NAME=$CORRECT_NAME" >>$SCRIPT_NAME
+echo "    export GIT_COMMITTER_EMAIL=$CORRECT_EMAIL" >>$SCRIPT_NAME
+echo fi >>$SCRIPT_NAME
+echo >>$SCRIPT_NAME
+echo 'if [ $GIT_AUTHOR_EMAIL = '"$OLD_EMAIL ]" >>$SCRIPT_NAME
+echo then >>$SCRIPT_NAME
+echo '    export GIT_AUTHOR_NAME='"$CORRECT_NAME" >>$SCRIPT_NAME
+echo '    export GIT_AUTHOR_EMAIL='"$CORRECT_EMAIL" >>$SCRIPT_NAME
+echo fi >>$SCRIPT_NAME
+echo '" --tag-name-filter cat -- --branches --tags' >>$SCRIPT_NAME
+
+echo "Executing script $SCRIPT_NAME"
+cat $SCRIPT_NAME
+sh $SCRIPT_NAME
