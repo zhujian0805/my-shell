6d6ceb7cbd7e11de8c2eb2bd00b3abbcbfd885a4
James Zhu <zhujian0805@gmail.com>
2017-08-25 09:14:10 +0800

    This commit has the following change(s): ------------------------------------------------  M loop-case.sh ------------------------------------------------

--- syntax/case/loop-case.sh
+++ syntax/case/loop-case.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-while [[ $# -ge 1 ]]; do                                                        
-  arg="$1"                                                                      
-  case $arg in                                                                  
-     -p|--profile)                                                             
-       shift; PROFILE_SHORT="$1"; shift                                        
+while [[ $# -ge 1 ]]; do
+  arg="$1"
+  case $arg in
+     -p|--profile)
+       shift; PROFILE_SHORT="$1"; shift
        echo $PROFILE_SHORT
-       ;;                                                                      
-     -t|--template)                                                            
-       shift; TEMPLATE_SHORT="$1"; shift                                       
+       ;;
+     -t|--template)
+       shift; TEMPLATE_SHORT="$1"; shift
        echo $TEMPLATE_SHORT
-       ;;                                                                      
-     --cn|--china)                                                             
-       shift; CHINA=true                                                       
+       ;;
+     --cn|--china)
+       shift; CHINA=true
        echo $CHINA
-       ;;                                                                      
-     *)                                                                        
-       echo "${USAGE}"; exit 1                                                 
-       ;;                                                                      
-  esac                                                                          
-done                    
+       ;;
+     *)
+       echo "${USAGE}"; exit 1
+       ;;
+  esac
+done
