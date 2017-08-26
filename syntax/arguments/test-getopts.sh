794406945e45913f0443e9617b82217b1fb578c7
James Zhu <zhujian0805@gmail.com>
2017-08-26 21:04:52 +0800

    This commit has the following change(s): ------------------------------------------------ A  test-getopts.sh ------------------------------------------------

---
+++ syntax/arguments/test-getopts.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+while getopts h:ms option
+do 
+    case "$option" in
+        h)
+            echo "option:h, value $OPTARG"
+            echo "next arg index:$OPTIND";;
+        m)
+            echo "option:m"
+            echo "next arg index:$OPTIND";;
+        s)
+            echo "option:s"
+            echo "next arg index:$OPTIND";;
+        \?)
+            echo "Usage: args [-h n] [-m] [-s]"
+            echo "-h means hours"
+            echo "-m means minutes"
+            echo "-s means seconds"
+            exit 1;;
+    esac
+done
+
+echo "*** do something now ***"
