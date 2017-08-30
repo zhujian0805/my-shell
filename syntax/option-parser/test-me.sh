79b81d8770c552f3cd6c550ac357bc0f2a4792eb
James Zhu <zhujian0805@gmail.com>
2017-08-30 12:20:09 +0800

    This commit has the following change(s): ------------------------------------------------  M test-me.sh ------------------------------------------------

--- syntax/option-parser/test-me.sh
+++ syntax/option-parser/test-me.sh
@@ -1,3 +1,6 @@
+# Some examples here
+# http://www.bahmanm.com/blogs/command-line-options-how-to-parse-in-bash-using-getopt
+
 # The usage of optional argument are as following
 bash test-optional-argument.sh -ak -b FUCK
 bash test-optional-argument.sh --opt_argument=k -b FUCK
