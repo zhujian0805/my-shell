3cabbdc219bb0eebeef957339d3332e50e3669d9
James Zhu <zhujian0805@gmail.com>
2017-09-03 17:32:45 +0800

    This commit has the following change(s): ------------------------------------------------  M color-echo.sh ------------------------------------------------

--- common-scripts/color-echo.sh
+++ common-scripts/color-echo.sh
@@ -1,32 +1,32 @@
 ## blue to echo 
 blue_echo () {
-    echo -e "\033[34m[ $1 ]\033[0m"
+    echo -e "\033[34m$1\033[0m"
 }
 
 ## green to echo 
 green_echo () {
-    echo -e "\033[32m[ $1 ]\033[0m"
+    echo -e "\033[32m$1\033[0m"
 }
 
 ## Error to warning with blink
 bred_echo () {
-    echo -e "\033[31m\033[01m\033[05m[ $1 ]\033[0m"
+    echo -e "\033[31m\033[01m\033[05m$1\033[0m"
 }
 
 ## Error to warning with blink
 byellow_echo () {
-    echo -e "\033[33m\033[01m\033[05m[ $1 ]\033[0m"
+    echo -e "\033[33m\033[01m\033[05m$1\033[0m"
 }
 
 redback_echo () {
-    echo -e "\033[41;37m[ $1 ]\033[0m"
+    echo -e "\033[41;37m$1\033[0m"
 }
 ## Error
 red_echo () {
-    echo -e "\033[31m\033[01m[ $1 ]\033[0m"
+    echo -e "\033[31m\033[01m$1\033[0m"
 }
 
 ## warning
 yellow_echo () {
-    echo -e "\033[33m\033[01m[ $1 ]\033[0m"
+    echo -e "\033[33m\033[01m$1\033[0m"
 }
