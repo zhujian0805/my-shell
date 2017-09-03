dcdd3692bf5f79a0a4ed9aa596b7ad03b479a289
James Zhu <zhujian0805@gmail.com>
2017-09-03 18:40:06 +0800

    This commit has the following change(s): ------------------------------------------------  M color-echo.sh ------------------------------------------------

--- common-scripts/color-echo.sh
+++ common-scripts/color-echo.sh
@@ -1,5 +1,31 @@
 # Examples:
 # http://blog.csdn.net/lidonghat/article/details/60584573
+
+function colorme()  
+{  
+    case "$1" in  
+        red)    nn="31";;  
+        green)  nn="32";;  
+        yellow) nn="33";;  
+        blue)   nn="34";;  
+        purple) nn="35";;  
+        cyan)   nn="36";;  
+    esac  
+    ff=""  
+    case "$2" in  
+        bold)   ff=";1";;  
+        bright) ff=";2";;  
+        uscore) ff=";4";;  
+        blink)  ff=";5";;  
+        invert) ff=";7";;  
+    esac  
+    color_begin=`echo -e -n "\033[${nn}${ff}m"`  
+    color_end=`echo -e -n "\033[0m"`  
+    while read line; do  
+        echo "${color_begin}${line}${color_end}"  
+    done  
+}
+
 ## blue to echo 
 blue_echo () {
     echo -e "\033[34m$@\033[0m"
