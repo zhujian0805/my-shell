599a90ec71b5f4e46a38cec30d0d528acbfc96dc
James Zhu <zhujian0805@gmail.com>
2018-05-06 12:05:40 +0800

    This commit has the following change(s): ------------------------------------------------ A  net-check.sh R  ../socket-reachable-test.sh -> ../probe/socket-reachable-test.sh R  ../tcp-probe.sh -> ../probe/tcp-probe.sh R  ../tcp-scanner.sh -> ../probe/tcp-scanner.sh D  ../ssh-login.expect ------------------------------------------------

---
+++ administration/network-diagnosis/net-check.sh
@@ -0,0 +1,31 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: net-check.sh
+# 
+#         USAGE: ./net-check.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: James Zhu (), zhujian0805@gmail.com
+#  ORGANIZATION: ZJ
+#       CREATED: 05/06/2018 12:01
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+if ETHPCI=$(lspci |grep -i eth)
+then
+    echo "Ethnet PCI devices is present:"
+    echo "----------------------------------------------"
+    echo "$ETHPCI"
+fi
+
+ifconfig
+
+route -n
