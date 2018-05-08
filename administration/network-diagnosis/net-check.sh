2de5e90c3c669f4ac1120d3ed2a89123bd56238c
James Zhu <zhujian0805@gmail.com>
2018-05-08 16:24:20 +0800

    This commit has the following change(s): ------------------------------------------------  M administration/network-diagnosis/net-check.sh ------------------------------------------------

--- administration/network-diagnosis/net-check.sh
+++ administration/network-diagnosis/net-check.sh
@@ -19,24 +19,40 @@
 
 set -o nounset                              # Treat unset variables as an error
 
+# Check hardware and drivers
 lspci |grep -i ether 
-
 dmesg|grep -i ether
 
+# Check ping OK?
 ping -c 3 8.8.8.8
 
+# Check IP address configured properly
 ifconfig
 
+# Check route table correct
 route -n
 
+# Check dns resovable 
 host baidu.com
 
+# Check if nscd has bad cache
+systemctl status nscd
+
+# Check host name is right?
 hostname
+cat /etc/hosts
 
-ping -c3 `hostname`
+# Ping host
+ping -c3 $(hostname)
 
+# Check traceroute path OK?
 traceroute -I baidu.com
 
+# Check processes are listening on ports
 netstat -tulpa|head -20
 
+# Check if SELinux is enabled
 sestatus
+
+
+
